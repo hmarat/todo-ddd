5aad73279322d394a690ec7265594d83898c350f
Marat Hakobjanyan <marat.hakobjanyan9@gmail.com>
2022-05-12 21:55:01 +0400

    feat: auth facade interface

--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -1,15 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd/domain/auth/auth_failures.dart';
 import 'package:ddd/domain/auth/value_objects.dart';
 
 abstract class IAuthFacade {
-  Future<void> registerWithEmailAndPassword({
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
     required EmailAddress emailAddress,
     required Password password,
   });
 
-  Future<void> signInWithEmailAndPassword({
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
     required EmailAddress emailAddress,
     required Password password,
   });
 
-  Future<void> signInWithGoogle();
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
 }
